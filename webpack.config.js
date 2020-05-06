90bab8462de9d7c542b16fd41883fe38260860cc
fajardm <fajar.dwi.mawan@gmail.com>
2020-05-06 22:50:27 +0700

    update webpack output library name

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js',
-        library: 'Paragraph',
+        library: 'Underline',
         libraryTarget: 'umd'
     }
 };
