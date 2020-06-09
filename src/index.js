aef99149296015ea379747df6f08639a35efe043
Peter Savchenko <specc.dev@gmail.com>
2020-06-09 18:02:43 +0300

    upd icon, codestyle

--- src/index.js
+++ src/index.js
@@ -9,178 +9,181 @@ require('./index.css').toString();
  * Allows to wrap inline fragment and style it somehow.
  */
 class Underline {
+  /**
+   * Class name for term-tag
+   *
+   * @type {string}
+   */
+  static get CSS() {
+    return 'cdx-underline';
+  };
+
+  /**
+   * @param {{api: object}}  - Editor.js API
+   */
+  constructor({ api }) {
+    this.api = api;
+
     /**
-     * Class name for term-tag
+     * Toolbar Button
+     *
+     * @type {HTMLElement|null}
+     */
+    this.button = null;
+
+    /**
+     * Tag represented the term
      *
      * @type {string}
      */
-    static get CSS() {
-        return 'cdx-underline';
+    this.tag = 'U';
+
+    /**
+     * CSS classes
+     */
+    this.iconClasses = {
+      base: this.api.styles.inlineToolButton,
+      active: this.api.styles.inlineToolButtonActive,
     };
+  }
 
-    /**
-     * @param {{api: object}}  - Editor.js API
-     */
-    constructor({api}) {
-        this.api = api;
+  /**
+   * Specifies Tool as Inline Toolbar Tool
+   *
+   * @returns {boolean}
+   */
+  static get isInline() {
+    return true;
+  }
 
-        /**
-         * Toolbar Button
-         *
-         * @type {HTMLElement|null}
-         */
-        this.button = null;
+  /**
+   * Create button element for Toolbar
+   *
+   * @returns {HTMLElement}
+   */
+  render() {
+    this.button = document.createElement('button');
+    this.button.type = 'button';
+    this.button.classList.add(this.iconClasses.base);
+    this.button.innerHTML = this.toolboxIcon;
 
-        /**
-         * Tag represented the term
-         *
-         * @type {string}
-         */
-        this.tag = 'U';
+    return this.button;
+  }
 
-        /**
-         * CSS classes
-         */
-        this.iconClasses = {
-            base: this.api.styles.inlineToolButton,
-            active: this.api.styles.inlineToolButtonActive
-        };
+  /**
+   * Wrap/Unwrap selected fragment
+   *
+   * @param {Range} range - selected fragment
+   */
+  surround(range) {
+    if (!range) {
+      return;
     }
 
+    const termWrapper = this.api.selection.findParentTag(this.tag, Underline.CSS);
+
     /**
-     * Specifies Tool as Inline Toolbar Tool
+     * If start or end of selection is in the highlighted block
+     */
+    if (termWrapper) {
+      this.unwrap(termWrapper);
+    } else {
+      this.wrap(range);
+    }
+  }
+
+  /**
+   * Wrap selection with term-tag
+   *
+   * @param {Range} range - selected fragment
+   */
+  wrap(range) {
+    /**
+     * Create a wrapper for highlighting
+     */
+    const u = document.createElement(this.tag);
+
+    u.classList.add(Underline.CSS);
+
+    /**
+     * SurroundContent throws an error if the Range splits a non-Text node with only one of its boundary points
      *
-     * @return {boolean}
-     */
-    static get isInline() {
-        return true;
-    }
-
-    /**
-     * Create button element for Toolbar
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Range/surroundContents}
      *
-     * @return {HTMLElement}
+     * // range.surroundContents(span);
      */
-    render() {
-        this.button = document.createElement('button');
-        this.button.type = 'button';
-        this.button.classList.add(this.iconClasses.base);
-        this.button.innerHTML = this.toolboxIcon;
-
-        return this.button;
-    }
+    u.appendChild(range.extractContents());
+    range.insertNode(u);
 
     /**
-     * Wrap/Unwrap selected fragment
-     *
-     * @param {Range} range - selected fragment
+     * Expand (add) selection to highlighted block
      */
-    surround(range) {
-        if (!range) {
-            return;
-        }
+    this.api.selection.expandToTag(u);
+  }
 
-        let termWrapper = this.api.selection.findParentTag(this.tag, Underline.CSS);
+  /**
+   * Unwrap term-tag
+   *
+   * @param {HTMLElement} termWrapper - term wrapper tag
+   */
+  unwrap(termWrapper) {
+    /**
+     * Expand selection to all term-tag
+     */
+    this.api.selection.expandToTag(termWrapper);
 
-        /**
-         * If start or end of selection is in the highlighted block
-         */
-        if (termWrapper) {
-            this.unwrap(termWrapper);
-        } else {
-            this.wrap(range);
-        }
-    }
+    const sel = window.getSelection();
+    const range = sel.getRangeAt(0);
+
+    const unwrappedContent = range.extractContents();
 
     /**
-     * Wrap selection with term-tag
-     *
-     * @param {Range} range - selected fragment
+     * Remove empty term-tag
      */
-    wrap(range) {
-        /**
-         * Create a wrapper for highlighting
-         */
-        let u = document.createElement(this.tag);
-
-        u.classList.add(Underline.CSS);
-
-        /**
-         * SurroundContent throws an error if the Range splits a non-Text node with only one of its boundary points
-         * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Range/surroundContents}
-         *
-         * // range.surroundContents(span);
-         */
-        u.appendChild(range.extractContents());
-        range.insertNode(u);
-
-        /**
-         * Expand (add) selection to highlighted block
-         */
-        this.api.selection.expandToTag(u);
-    }
+    termWrapper.parentNode.removeChild(termWrapper);
 
     /**
-     * Unwrap term-tag
-     *
-     * @param {HTMLElement} termWrapper - term wrapper tag
+     * Insert extracted content
      */
-    unwrap(termWrapper) {
-        /**
-         * Expand selection to all term-tag
-         */
-        this.api.selection.expandToTag(termWrapper);
-
-        let sel = window.getSelection();
-        let range = sel.getRangeAt(0);
-
-        let unwrappedContent = range.extractContents();
-
-        /**
-         * Remove empty term-tag
-         */
-        termWrapper.parentNode.removeChild(termWrapper);
-
-        /**
-         * Insert extracted content
-         */
-        range.insertNode(unwrappedContent);
-
-        /**
-         * Restore selection
-         */
-        sel.removeAllRanges();
-        sel.addRange(range);
-    }
+    range.insertNode(unwrappedContent);
 
     /**
-     * Check and change Term's state for current selection
+     * Restore selection
      */
-    checkState() {
-        const termTag = this.api.selection.findParentTag(this.tag, Underline.CSS);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
 
-        this.button.classList.toggle(this.iconClasses.active, !!termTag);
-    }
+  /**
+   * Check and change Term's state for current selection
+   */
+  checkState() {
+    const termTag = this.api.selection.findParentTag(this.tag, Underline.CSS);
 
-    /**
-     * Get Tool icon's SVG
-     * @return {string}
-     */
-    get toolboxIcon() {
-        return require('./assets/underline.svg').default;
-    }
+    this.button.classList.toggle(this.iconClasses.active, !!termTag);
+  }
 
-    /**
-     * Sanitizer rule
-     * @return {{mark: {class: string}}}
-     */
-    static get sanitize() {
-        return {
-            mark: {
-                class: Underline.CSS
-            }
-        };
-    }
+  /**
+   * Get Tool icon's SVG
+   *
+   * @returns {string}
+   */
+  get toolboxIcon() {
+    return require('./assets/underline.svg').default;
+  }
+
+  /**
+   * Sanitizer rule
+   *
+   * @returns {{u: {class: string}}}
+   */
+  static get sanitize() {
+    return {
+      u: {
+        class: Underline.CSS,
+      },
+    };
+  }
 }
 
 module.exports = Underline;
